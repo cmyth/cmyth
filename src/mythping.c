3d3a4fc9db5b2ee44d75febbdc408687d962453d
Jon Gettler <gettler@mvpmc.org>
2009-11-10 20:06:05 -0600

    Allow mythping to display information about the recordings.

--- src/mythping.c
+++ src/mythping.c
@@ -51,6 +51,78 @@ is_alive(char *host)
 	return 1;
 }
 
+static int
+get_recordings(int level)
+{
+	cmyth_proglist_t episodes;
+	int count, i;
+
+	episodes = cmyth_proglist_get_all_recorded(control);
+
+	if (episodes == NULL) {
+		return -1;
+	}
+
+	count = cmyth_proglist_get_count(episodes);
+
+	for (i=0; i<count; i++) {
+		char *title;
+		char *subtitle=NULL, *channel;
+		char *description=NULL, *category=NULL, *recgroup=NULL;
+		cmyth_proginfo_t prog;
+
+		prog = cmyth_proglist_get_item(episodes, i);
+
+		title = cmyth_proginfo_title(prog);
+
+		if (level > 2) {
+			subtitle = cmyth_proginfo_subtitle(prog);
+			channel = cmyth_proginfo_channame(prog);
+		}
+
+		if (level > 3) {
+			description = cmyth_proginfo_description(prog);
+			category = cmyth_proginfo_category(prog);
+			recgroup = cmyth_proginfo_recgroup(prog);
+		}
+
+		if (channel) {
+			printf("\tChannel:         %s\n", channel);
+		}
+		if (title) {
+			printf("\tTitle:           %s\n", title);
+		}
+		if (subtitle) {
+			printf("\tSubtitle:        %s\n", subtitle);
+		}
+		if (description) {
+			printf("\tDescription:     %s\n", description);
+		}
+		if (category) {
+			printf("\tCategory:        %s\n", category);
+		}
+		if (recgroup) {
+			printf("\tRecording Group: %s\n", recgroup);
+		}
+
+
+		printf("\n");
+
+		ref_release(channel);
+		ref_release(title);
+		ref_release(subtitle);
+		ref_release(description);
+		ref_release(category);
+		ref_release(recgroup);
+
+		ref_release(prog);
+	}
+
+	ref_release(episodes);
+
+	return count;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -65,7 +137,7 @@ main(int argc, char **argv)
 			exit(0);
 			break;
 		case 'v':
-			verbose = 1;
+			verbose++;
 			break;
 		default:
 			print_help(argv[0]);
@@ -102,6 +174,10 @@ main(int argc, char **argv)
 		printf("\trecordings: %d\n", count);
 
 		ref_release(list);
+	}
+
+	if (verbose > 1) {
+		get_recordings(verbose);
 	}
 
 	ref_release(control);
