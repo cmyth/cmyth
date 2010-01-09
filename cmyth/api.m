9500bbdbd9b47133560f9e27dceda1c861b27dd3
Jon Gettler <gettler@mvpmc.org>
2010-01-09 14:22:08 -0600

    Add the GNU LGPL v2.1 header to api.h and api.m.

--- cmyth/api.m
+++ cmyth/api.m
@@ -1,10 +1,21 @@
-//
-//  api.m
-//  cmyth
-//
-//  Created by Jon Gettler on 12/17/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/*
+ *  Copyright (C) 2009-2010, Jon Gettler
+ *  http://www.mvpmc.org/
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <sys/socket.h>
 #include <netinet/in.h>
